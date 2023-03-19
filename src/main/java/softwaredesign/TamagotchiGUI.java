ca3eb7f99df6d6effd3632a25574c8e8a2cc6ba9
seoyeonnk <kkimseoyeon@snu.ac.kr>
2023-03-19 14:45:04 +0100

    vitals update

--- src/main/java/softwaredesign/TamagotchiGUI.java
+++ src/main/java/softwaredesign/TamagotchiGUI.java
@@ -4,7 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TamagotchiGUI extends JFrame {
-
+    private JPanel jp1;
+    private JLabel vital;
+    private JProgressBar hungerVital;
+    private JProgressBar cleanVital;
+    private JProgressBar moodVital;
+    private JLabel stLabel;
     private JLabel statusLabel;
     private JButton feedButton;
     private JButton playButton;
@@ -16,9 +21,33 @@ public class TamagotchiGUI extends JFrame {
         // Set the layout manager for the frame
         setLayout(new BorderLayout());
 
+        jp1 = new JPanel();
+
+        vital = new JLabel("Vitals ");
+        jp1.add(vital);
+
+        hungerVital = new JProgressBar(0, 100);
+        hungerVital.setValue(50);
+        hungerVital.setStringPainted(true);
+        hungerVital.setString("Hunger");
+        jp1.add(hungerVital);
+
+        cleanVital = new JProgressBar(0, 100);
+        cleanVital.setValue(50);
+        cleanVital.setStringPainted(true);
+        cleanVital.setString("Cleanliness");
+        jp1.add(cleanVital);
+
+        moodVital = new JProgressBar(0, 100);
+        moodVital.setValue(50);
+        moodVital.setStringPainted(true);
+        moodVital.setString("Mood");
+        jp1.add(moodVital);
+
+        add(jp1, BorderLayout.NORTH);
         // Create the status label and add it to the top of the frame
         statusLabel = new JLabel("I'm hungry!");
-        add(statusLabel, BorderLayout.NORTH);
+        add(statusLabel, BorderLayout.CENTER);
 
         // Create the buttons and add them to the center of the frame
         feedButton = new JButton("Feed");
@@ -28,7 +57,7 @@ public class TamagotchiGUI extends JFrame {
         buttonPanel.add(feedButton);
         buttonPanel.add(playButton);
         buttonPanel.add(sleepButton);
-        add(buttonPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
 
         // Set the size of the frame and make it visible
         setSize(600,500);
